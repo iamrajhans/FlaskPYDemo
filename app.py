6d97f5df043baf4883b35914f0b627d4bbbeb4fe
iamrajhans <rajhans.jadhao@gmail.com>
2015-12-30 08:44:18 +0530

    new route to get tasks and working

--- app.py
+++ app.py
@@ -2,11 +2,22 @@ from flask import *
 
 app=Flask(__name__)
 
+
+tasks=[
+        {
+          'id':1,
+          'title':u'This is a test',
+          'desc':u'flask is awesome'    
+        }   
+     ]
 @app.route('/')
 
 def index():
     return "hello world"
 
+@app.route('/todo/tasks',methods=['GET'])
+def get_task():
+    return jsonify({'tasks':tasks})
 
 if __name__ == '__main__':
     app.run(debug=True)
