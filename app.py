21936908520f0cec486994dc03f7e38fcbff4aa1
iamrajhans <rajhans.jadhao@gmail.com>
2015-12-31 00:26:26 +0530

    added new route for id

--- app.py
+++ app.py
@@ -19,5 +19,12 @@ def index():
 def get_task():
     return jsonify({'tasks':tasks})
 
+@app.route('/todo/tasks/<int:task_id>',methods=['GET'])
+def get_tasks(task_id):
+    task=[task for task in tasks if task['id']==task_id]
+    if len(task)==0:
+        abort(404)
+    return jsonify({'task':task[0]})
+
 if __name__ == '__main__':
     app.run(debug=True)
